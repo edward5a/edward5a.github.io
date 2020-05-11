5d4e8dc0d244b444eb1083517ccb9ec0f47bc890
edward5a <gundam4b@gmail.com>
2020-05-12 03:08:00 +0800

    avoidarea+destination functions

--- index.js
+++ index.js
@@ -8,18 +8,38 @@
  *
  * @param   {H.service.Platform} platform    A stub class to access HERE services
  */
-function calculateRouteFromAtoB (platform) {
-    var router = platform.getRoutingService(),
-      routeRequestParams = {
+function calculateRouteFromAtoB (platform,avoidArea,destination) {
+    var router = platform.getRoutingService();
+    console.log(avoidArea==null);
+    if(avoidArea != null){
+      var routeRequestParams = {
         mode: 'fastest;car',
         representation: 'display',
         routeattributes : 'waypoints,summary,shape,legs',
         maneuverattributes: 'direction,action',
         waypoint0: '22.3712,114.1782', // Brandenburg Gate
-        waypoint1: '22.3062049,114.21309471',  // Friedrichstraße Railway Station
-        avoidareas: '22.3572906,114.181835;22.3461169,114.1729177'
+        //waypoint1: '22.3062049,114.21309471',  // Friedrichstraße Railway Station
+        waypoint1: destination.latitude+','+destination.longitude,
+        //avoidareas: '22.3572906,114.181835;22.3461169,114.1729177'
+        avoidareas: avoidArea.NorthEastLat+','+avoidArea.NorthEastLng+';'+avoidArea.SouthWestLat+','+avoidArea.SouthWestLng,
+      };      
+
+    }
+    else{
+      var routeRequestParams = {
+        mode: 'fastest;car',
+        representation: 'display',
+        routeattributes : 'waypoints,summary,shape,legs',
+        maneuverattributes: 'direction,action',
+        waypoint0: '22.3712,114.1782', // Brandenburg Gate
+        //waypoint1: '22.3062049,114.21309471',  // Friedrichstraße Railway Station
+        waypoint1: destination.latitude+','+destination.longitude,
+        //avoidareas: '22.3572906,114.181835;22.3461169,114.1729177'
+        //avoidareas: avoidArea.NorthEastLat+','+avoidArea.NorthEastLon+','+avoidArea.SouthWestLat+','+avoidArea.SouthWestLong,
       };
-  
+    }
+
+      console.log(routeRequestParams);
   
     router.calculateRoute(
       routeRequestParams,
@@ -40,12 +60,10 @@ function calculateRouteFromAtoB (platform) {
     * A representitive styling can be found the full JS + HTML code of this example
     * in the functions below:
     */
+    clearOldSuggestions();
     addRouteShapeToMap(route);
     addManueversToMap(route);
   
-    addWaypointsToPanel(route.waypoint);
-    addManueversToPanel(route);
-    addSummaryToPanel(route.summary);
     // ... etc.
   }
   
@@ -63,8 +81,7 @@ function calculateRouteFromAtoB (platform) {
   
   // set up containers for the map  + panel
   var mapContainer = document.getElementById('map'),
-    routeInstructionsContainer = document.getElementById('panel');
-  
+    suggestionsContainer = document.getElementById('panel');
   //Step 1: initialize communication with the platform
   // In your own code, replace variable window.apikey with your own apikey
   var platform = new H.service.Platform({
@@ -76,7 +93,7 @@ function calculateRouteFromAtoB (platform) {
   //Step 2: initialize a map - this map is centered over Berlin
   var map = new H.Map(mapContainer,
     defaultLayers.vector.normal.map,{
-    center: {lat:52.5160, lng:13.3779},
+    center: {lat:22.3712, lng:114.1782},
     zoom: 13,
     pixelRatio: window.devicePixelRatio || 1
   });
@@ -94,6 +111,9 @@ function calculateRouteFromAtoB (platform) {
   // Hold a reference to any infobubble opened
   var bubble;
   
+  var avoidArea = null;
+  var destination = null;
+
   /**
    * Opens/Closes a infobubble
    * @param  {H.geo.Point} position     The location on the map.
@@ -118,10 +138,11 @@ function calculateRouteFromAtoB (platform) {
    * Creates a H.map.Polyline from the shape of the route and adds it to the map.
    * @param {Object} route A route as received from the H.service.RoutingService
    */
+  var polyline;
   function addRouteShapeToMap(route){
     var lineString = new H.geo.LineString(),
-      routeShape = route.shape,
-      polyline;
+      routeShape = route.shape;
+      //polyline;
   
     routeShape.forEach(function(point) {
       var parts = point.split(',');
@@ -147,6 +168,7 @@ function calculateRouteFromAtoB (platform) {
    * Creates a series of H.map.Marker points from the route and adds them to the map.
    * @param {Object} route  A route as received from the H.service.RoutingService
    */
+  var group = new H.map.Group();
   function addManueversToMap(route){
     var svgMarkup = '<svg width="18" height="18" ' +
       'xmlns="http://www.w3.org/2000/svg">' +
@@ -154,7 +176,7 @@ function calculateRouteFromAtoB (platform) {
         'fill="#1b468d" stroke="white" stroke-width="1"  />' +
       '</svg>',
       dotIcon = new H.map.Icon(svgMarkup, {anchor: {x:8, y:8}}),
-      group = new  H.map.Group(),
+      //group = new  H.map.Group(),
       i,
       j;
   
@@ -183,91 +205,127 @@ function calculateRouteFromAtoB (platform) {
     map.addObject(group);
   }
   
-  
-  /**
-   * Creates a series of H.map.Marker points from the route and adds them to the map.
-   * @param {Object} route  A route as received from the H.service.RoutingService
-   */
-  function addWaypointsToPanel(waypoints){
-  
-  
-  
-    var nodeH3 = document.createElement('h3'),
-      waypointLabels = [],
-      i;
-  
-  
-     for (i = 0;  i < waypoints.length; i += 1) {
-      waypointLabels.push(waypoints[i].label)
-     }
-  
-     nodeH3.textContent = waypointLabels.join(' - ');
-  
-    routeInstructionsContainer.innerHTML = '';
-    routeInstructionsContainer.appendChild(nodeH3);
-  }
-  
-  /**
-   * Creates a series of H.map.Marker points from the route and adds them to the map.
-   * @param {Object} route  A route as received from the H.service.RoutingService
-   */
-  function addSummaryToPanel(summary){
-    var summaryDiv = document.createElement('div'),
-     content = '';
-     content += '<b>Total distance</b>: ' + summary.distance  + 'm. <br/>';
-     content += '<b>Travel Time</b>: ' + summary.travelTime.toMMSS() + ' (in current traffic)';
-  
-  
-    summaryDiv.style.fontSize = 'small';
-    summaryDiv.style.marginLeft ='5%';
-    summaryDiv.style.marginRight ='5%';
-    summaryDiv.innerHTML = content;
-    routeInstructionsContainer.appendChild(summaryDiv);
-  }
-  
-  /**
-   * Creates a series of H.map.Marker points from the route and adds them to the map.
-   * @param {Object} route  A route as received from the H.service.RoutingService
-   */
-  function addManueversToPanel(route){
-  
-  
-  
-    var nodeOL = document.createElement('ol'),
-      i,
-      j;
-  
-    nodeOL.style.fontSize = 'small';
-    nodeOL.style.marginLeft ='5%';
-    nodeOL.style.marginRight ='5%';
-    nodeOL.className = 'directions';
-  
-       // Add a marker for each maneuver
-    for (i = 0;  i < route.leg.length; i += 1) {
-      for (j = 0;  j < route.leg[i].maneuver.length; j += 1) {
-        // Get the next maneuver.
-        maneuver = route.leg[i].maneuver[j];
-  
-        var li = document.createElement('li'),
-          spanArrow = document.createElement('span'),
-          spanInstruction = document.createElement('span');
-  
-        spanArrow.className = 'arrow '  + maneuver.action;
-        spanInstruction.innerHTML = maneuver.instruction;
-        li.appendChild(spanArrow);
-        li.appendChild(spanInstruction);
-  
-        nodeOL.appendChild(li);
+  var GETBOUNTARY_URL = 'https://maps.googleapis.com/maps/api/geocode/json',
+    ajaxRequest = new XMLHttpRequest(),
+    address = '';
+
+
+  function clearOldSuggestions(){
+      group.removeAll ();
+      if(bubble){
+          bubble.close();
       }
-    }
-  
-    routeInstructionsContainer.appendChild(nodeOL);
+      if(map&&polyline){
+        map.removeObject(polyline);
+      }
+  }
+
+  function getGeometryLocation(textBox, event){
+    if (address != textBox.value){
+      if (textBox.value.length >= 1){
+
+          /**
+           * A full list of available request parameters can be found in the Geocoder Autocompletion
+           * API documentation.
+           *
+           */
+          var params = '?' +
+              'address=' +  encodeURIComponent(textBox.value) +   // The search text which is the basis of the query
+              '&key=AIzaSyBGmGio79beBlUU1HMqPn36yuunILthoAQ';
+          ajaxRequest.open('GET', GETBOUNTARY_URL + params );
+          ajaxRequest.send();
+      }
+  }
+  address = textBox.value;
   }
   
-  
+  function onAutoCompleteSuccess() {
+    /*
+     * The styling of the suggestions response on the map is entirely under the developer's control.
+     * A representitive styling can be found the full JS + HTML code of this example
+     * in the functions below:
+     */
+    //addSuggestionsToPanel(this.response);  // In this context, 'this' means the XMLHttpRequest itself.
+    addBoundaryToPanel(this.response);
+    //console.dir(this.response);
+}
+
+function onAutoCompleteFailed() {
+  alert('Ooops!');
+}
+
+function addSuggestionsToPanel(response){
+  var suggestions = document.getElementById('suggestions');
+  suggestions.innerHTML = JSON.stringify(response, null, ' ');
+}
+
+
+function addBoundaryToPanel(response){  
+  if(isAvoidArea){
+    avoidArea  = 
+    {NorthEastLat: response.results[0].geometry.viewport.northeast.lat,
+    NorthEastLng: response.results[0].geometry.viewport.northeast.lng,
+    SouthWestLat: response.results[0].geometry.viewport.southwest.lat,
+    SouthWestLng: response.results[0].geometry.viewport.southwest.lng,
+    };
+    console.log(avoidArea);
+  }
+  if(isDestination){
+    destination =
+    {
+      latitude: response.results[0].geometry.location.lat,
+      longitude: response.results[0].geometry.location.lng,
+    }
+  }
+}
+/**
+ * This function will be called if a communication error occurs during the XMLHttpRequest
+ */
+
+// Attach the event listeners to the XMLHttpRequest object
+ajaxRequest.addEventListener("load", onAutoCompleteSuccess);
+ajaxRequest.addEventListener("error", onAutoCompleteFailed);
+ajaxRequest.responseType = "json";
+
+
   Number.prototype.toMMSS = function () {
     return  Math.floor(this / 60)  +' minutes '+ (this % 60)  + ' seconds.';
   }
   
+  var content =  '<strong style="font-size: large;">' + 'The Location You Want to Avoid'  +'</strong></br>';
+  content  += '<br/><input type="text" id="avoidArea" style="margin-left:5%; margin-right:5%; min-width:90%"  onkeyup="return getGeometryLocation(this, event);"><br/>';
+  content  +=  '<strong style="font-size: large;">' + 'Destination'  +'</strong></br>';
+  content  += '<br/><input type="text" id="destination" style="margin-left:5%; margin-right:5%; min-width:90%"  onkeyup="return getGeometryLocation(this, event);"><br/>';
+  //content  += '<br/><strong>Response:</strong><br/>';
+  //content  += '<div style="margin-left:5%; margin-right:5%;"><pre ><code  id="suggestions" style="font-size: small;">' +'{}' + '</code></pre></div>';
+  suggestionsContainer.innerHTML = content;
+
+ var isAvoidArea = new Boolean(false);
+ var isDestination = new Boolean(false);
+
+ $("#avoidArea").on("input", function() {
+
+  if ($(this).val() == "") {
+    isAvoidArea = false;
+  }
+  else {
+    isAvoidArea = true;
+  }
+});
+
+$("#destination").on("input", function() {
+
+  if ($(this).val() == "") {
+    isDestination = false;
+  }
+  else {
+    isDestination = true;
+  }
+});
+
   // Now use the map as required...
-  calculateRouteFromAtoB (platform);
+  //calculateRouteFromAtoB (platform,avoidArea)
+  
+//document.getElementById("plot").onclick = calculateRouteFromAtoB (platform,avoidArea);
+
+
